70fe8a149f2987d1b0bf6f6a2aa36f59160a1fea
starterdragon <papadie102@gmail.com>
2018-08-28 13:11:15 +0800

    Update init.inc.php

--- core/init.inc.php
+++ core/init.inc.php
@@ -1,11 +1,14 @@
 
 <?php
+
 $servers = array(
   array('66.70.171.63', '25627'),
   array('playsakura.online', '25627'),
   array('sakura.pocketmc.net', '25627'),
   array('sakurahigh.net', '25627')
   );
+
+error_reporting(0);
 
 $path = dirname(__FILE__);
 
